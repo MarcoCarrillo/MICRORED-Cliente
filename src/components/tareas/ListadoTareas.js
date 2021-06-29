68c2490107170405358d672ed3cca6d906f8a4e6
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-06-29 14:07:12 -0500

    Animacion para tareas

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -2,6 +2,7 @@ import React, {Fragment, useContext} from 'react'
 import Tarea from '../tareas/Tarea';
 import proyectoContext from '../../context/proyectos/proyectoContext';
 import tareaContext from '../../context/tareas/tareaContext';
+import {CSSTransition, TransitionGroup} from 'react-transition-group/'
 
 const ListadoTareas = () => {
 
@@ -29,12 +30,21 @@ const ListadoTareas = () => {
             <h2>Proyecto: {proyectoActual.nombre}</h2>
             <ul className='listado-tareas'>
                 {tareasproyecto.length === 0 ? (<li className='tarea'>No hay tareas</li>) 
-                    : tareasproyecto.map(tarea =>(
-                        <Tarea 
+                    : 
+                    <TransitionGroup>
+                    {tareasproyecto.map(tarea =>(
+                        <CSSTransition
                             key={tarea.id}
-                            tarea={tarea}
-                        />
-                    )) 
+                            timeout={300}
+                            classNames='tarea'
+                        >
+                            <Tarea 
+                                
+                                tarea={tarea}
+                            />
+                        </CSSTransition>
+                    ))} 
+                    </TransitionGroup>
                 }
             </ul>
             <button
