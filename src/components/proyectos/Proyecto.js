afd6222b4d234c014b6228ec7ad4e4abdc1e302d
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-07-28 00:56:07 -0500

    Obtener un proyecto en activo
    Al seleccionar el proyecto se muestra su nombre, y tambien servira para mostrar las tareas ligadas a ese proyecto, en react developer tools (ProyectoState) se ve el cambio entre los datos de los proyectos al seleccionarlos

--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -23,7 +23,7 @@ const Proyecto = ({proyecto}) => {
             <button
                 type='button'
                 className='btn btn-blank'
-                onClick={()=>seleccionarProyecto(proyecto.id)}  //Si damos clic en uno de los proyectos corre la funcion, le pasa el id del proyecto actual, proyecto actual va al state y le pasa como payload el id, evalua el reducer y comienza a iterar entre cada proyecto hasta que encuentra el mismo id al que le dimos clic y lo va filtrar   
+                onClick={()=>seleccionarProyecto(proyecto._id)}  //Si damos clic en uno de los proyectos corre la funcion, le pasa el id del proyecto actual, proyecto actual va al state y le pasa como payload el id, evalua el reducer y comienza a iterar entre cada proyecto hasta que encuentra el mismo id al que le dimos clic y lo va filtrar   
             >
             {proyecto.nombre}</button>
         </li>
