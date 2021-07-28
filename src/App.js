bd3b72e6f1901cde5e20b3086abe88816530cf03
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-07-27 23:54:56 -0500

    Autenticar usuario aunque recargue la pagina
    La sesion permanecera aunque recargue la pagina si el token sigue activo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ import TareaState from './context/tareas/tareaState';
 import AlertaState from './context/alertas/alertaState';
 
 import AuthState from './context/autenticacion/authState';
+import tokenAuth from './config/token';
 
+//Revisar si tenemos un token
+const token = localStorage.getItem('token');
+if(token){
+  tokenAuth(token);
+}
 function App() {
   // console.log(process.env.REACT_APP_BACKEND_URL);
   return (
