61913ad8f27e363fc5f243ac6e11000458213d43
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-07-27 19:02:48 -0500

    Autenticar y redirigir al usuario
    En este cambio tambien se verifica si un usuario ya existe y muestra su alerta correspondiente actualizando el componente con el useEffect que depende que haya un mensaje que provenga del API, depende si el usuario esta autenticado y redirige al usuario a la pagina de proyectos cuando crea su cuenta con props.history

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,17 +1,29 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import AlertaContext from '../../context/alertas/alertaContext';
 import AuthContext from '../../context/autenticacion/authContext';
 
 
-const NuevaCuenta = () => {
+const NuevaCuenta = (props) => {
 
     //Extraer valores del context
     const alertaContext = useContext(AlertaContext);
     const {alerta, mostrarAlerta} = alertaContext;
 
     const authContext = useContext(AuthContext);
-    const {registrarUsuario} = authContext;
+    const {mensaje, autenticado, registrarUsuario} = authContext;
+
+    //En caso de que el usuario se haya autenticado o registrado o sea un registro duplicado
+    useEffect(() => {
+        if(autenticado){
+            props.history.push('/proyectos');
+        }
+        if(mensaje) {
+            mostrarAlerta(mensaje.msg, mensaje.categoria);
+            return;
+        }
+    },[mensaje, autenticado, props.history]); 
+
 
     //State para iniciar sesion
     const [usuario, guardarUsuario] = useState({
