b9e2ff4f0a951e7aeb99ea111fbc8863b4d4928f
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-07-28 19:30:14 -0500

    Proteccion del componente de proyectos

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import AlertaState from './context/alertas/alertaState';
 
 import AuthState from './context/autenticacion/authState';
 import tokenAuth from './config/token';
+import RutaPrivada from './components/rutas/RutaPrivada';
 
 //Revisar si tenemos un token
 const token = localStorage.getItem('token');
@@ -30,7 +31,7 @@ function App() {
                 <Route exact path ='/' component={LoginEmpresa} />
                 <Route exact path ='/login' component={Login} />
                 <Route exact path ='/nueva-cuenta' component={NuevaCuenta} />
-                <Route exact path ='/proyectos' component={Proyectos} />
+                <RutaPrivada exact path ='/proyectos' component={Proyectos} />
               </Switch>
             </Router>
           </AuthState>
